c7a5d46fbd5f6d0d2320048d4056b797de7d269b
mandarpitkar <139145085+mandarpitkar@users.noreply.github.com>
2023-08-29 20:28:53 -0300

    mudei arquivo

---
+++ script.js
@@ -0,0 +1,60 @@
+let catalogo = [{
+    id: 1,
+    nome: 'Kurta',
+    marca: 'ReiIndi',
+    preco: 700,
+    nomeArquivoImagem: 'produto-1.jpg'
+ },
+ {
+    id: 2,
+    marca: 'ReiIndi',
+    nome: 'Kurta',
+    preco: 670,
+    nomeArquivoImagem: 'produto-2.jpg'
+ },
+ {
+    id: 3,
+    marca:'ReiIndi',
+    nome: 'Kurta',
+    preco: 750,
+    nomeArquivoImagem: 'produto-3.jpg'
+ },
+ {
+    id: 4,
+    marca:'ReiIndi',
+    nome :'Saree',
+    preco :700,
+    nomeArquivoImagem: 'produto-4.jpg'
+},
+ {
+    id: 5,
+    marca :'ReiIndi',
+    nome :'Saree',
+    preco :800,
+    nomeArquivoImagem: 'produto-5.jpg'
+ },
+    {
+    id: 6,
+    marca :'ReiIndi',
+    nome :'Saree',
+    preco :900,
+    nomeArquivoImagem: 'produto-6.jpg'
+    },
+];
+
+for (let produtoCatalogo of catalogo) {
+let cartaoProduto =  `<div>
+<img src="./imagens/${produtoCatalogo.nomeArquivoImagem}" style="height: 300px;">
+<p>${produtoCatalogo.marca}</p>
+<p>${produtoCatalogo.nome}</p>
+<p>R$ ${produtoCatalogo.preco}</p>
+<button>Adicionar</button>
+</div>`;
+
+document.getElementById('container-produto').innerHTML += cartaoProduto
+}
+
+
+
+
+
